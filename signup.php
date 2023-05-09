ec7d0c51743d4348ed4e7bae4b9e3743e037a4a5
Adam Young <adamyoungy678@gmail.com>
2023-05-09 15:52:29 +0100

    initial commit

---
+++ signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Signup page</title>
+
+    <meta name="description" content="Sign up form">
+    <meta name="author" content="Adam Young">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+
+</head>
+<body>
+    <div class="container">
+
+    </div>
+
+</body>
+</html>
