119627e0765663b51309eddb080b8c0b1ac085ee
Adam Young <adamyoungy678@gmail.com>
2023-05-10 12:56:49 +0100

    feat: added success and error pop ups after registration

--- signup.php
+++ signup.php
@@ -1,4 +1,6 @@
 <?php
+$success = 0;
+$user = 0;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include 'connect.php';
@@ -11,14 +13,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     if ($stmt->rowCount() > 0) {
-        echo "User already exists";
+//        echo "User already exists";
+        $user = 1;
     } else {
         $sql = "insert into `registration`(username,password)
         values('$username', '$password')";
         $stmt = $pdo->prepare($sql);
         $result = $stmt->execute();
         if($result){
-            echo "Signup successful";
+//            echo "Signup successful";
+            $success = 1;
         }else{
             die($stmt->errorInfo()[2]);
         }
@@ -47,6 +51,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 </head>
 <body>
+
+<?php
+
+if($user){
+    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Ohh no Sorry </strong> User already exists
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>';
+}
+?>
+
+<?php
+
+if($success){
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Success </strong> You are successfully signed up!
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>';
+}
+?>
 <h1 class="text-center">Sign up form</h1>
 <div class="container mt-5">
     <form action="signup.php" method="post">
