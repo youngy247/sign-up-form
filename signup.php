a8d3e5d6f2bb18d2f99e2782e59faf8b555c2558
Adam Young <adamyoungy678@gmail.com>
2023-05-10 12:48:07 +0100

    feat: added 2 prepare statements for checking if the username already exists and if not then insert the details from the registration into the db

--- signup.php
+++ signup.php
@@ -1,8 +1,31 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD']=='POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include 'connect.php';
+    $pdo = connectToDb($DB);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "Select * from `registration` where username='$username'";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        echo "User already exists";
+    } else {
+        $sql = "insert into `registration`(username,password)
+        values('$username', '$password')";
+        $stmt = $pdo->prepare($sql);
+        $result = $stmt->execute();
+        if($result){
+            echo "Signup successful";
+        }else{
+            die($stmt->errorInfo()[2]);
+        }
+    }
 }
+
+
 
 ?>
 
@@ -18,25 +41,26 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     <meta name="description" content="Sign up form">
     <meta name="author" content="Adam Young">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
 
 </head>
 <body>
 <h1 class="text-center">Sign up form</h1>
-    <div class="container mt-5">
-        <form action="signup.php" method="post">
-            <div class="form-group">
-                <label for="exampleInputEmail1">Username</label>
-                <input type="text" class="form-control" placeholder="Enter your username" name="username">
-            </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" placeholder="Enter your password" name="password">
-            </div>
-            <button type="submit" class="btn btn-primary w-100">Submit</button>
-        </form>
-    </div>
+<div class="container mt-5">
+    <form action="signup.php" method="post">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Username</label>
+            <input type="text" class="form-control" placeholder="Enter your username" name="username">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" placeholder="Enter your password" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary w-100">Submit</button>
+    </form>
+</div>
 
 </body>
 </html>
